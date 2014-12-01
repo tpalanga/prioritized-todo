375d82cf84a53b592b265ee873de10f5d1e66bca
Tudor Palanga <tudor.palanga@gmail.com>
2014-12-01 18:17:45 +0100

    Add list controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api._
+import models.TodoItems
+import play.api.libs.json.Json
 import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def list = Action async {
+    TodoItems.all.map(list => Ok(Json.toJson(list)))
   }
 
 }
