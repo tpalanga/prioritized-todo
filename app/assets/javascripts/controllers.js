362c6edea8268b30ee1b84ea3c9dbd90de8d1e0e
Tudor Palanga <tudor.palanga@gmail.com>
2014-12-06 11:57:37 +0100

    Get item list from server  in 2 ways - with $http or $resource

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -2,8 +2,21 @@
     'use strict';
 
     /** Controllers */
-    angular.module('prioTodo.controllers', ['prioTodo.services']).
-        controller('TodoCtrl', function ($scope, $http, todoModel) {
-            $scope.todoItems = todoModel.getTodoItems();
+    angular.module('prioTodo.controllers', ['ngResource']).
+        controller('TodoCtrl', function ($scope, $http, $resource) {
+
+            $scope.getList = function() {
+                $http({method: 'GET', url: '/list'})
+                    .success(function(data) {
+                        $scope.todoItems = data;
+                    });
+            };
+
+            $scope.getListResource = function() {
+                var TodoItems = $resource('/list');
+                $scope.todoItems = TodoItems.query();
+            };
+
+            $scope.getList();
         });
 }());
